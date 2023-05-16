bd4a9eeb207c9e0f8dc19e864cd58b1a06f392f6
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2023-05-16 16:19:40 -0300

    feat: finalização das configurações base.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
+const prisma = new PrismaClient()
 
-app.get('/teste', () => {
-  return 'Olá'
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
 })
 
 app.listen({ port: 7474 }).then(() => {
